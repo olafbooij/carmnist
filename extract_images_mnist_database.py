d7b280a9baea98f654da2e840011a93e325b5283
O B thuis <olaf.booij@xs4all.nl>
2022-09-26 10:19:49 +0200

    added creation of directory if it does not exist

--- extract_images_mnist_database.py
+++ extract_images_mnist_database.py
@@ -1,7 +1,8 @@
 from keras.datasets import mnist
 from PIL import Image, ImageOps
+from pathlib import Path
 
-# Load the mnist dataset
+# Load the mnist dataset (caches in .keras/datasets/mnist.npz)
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
 
 # Extract and save image as a file
@@ -15,8 +16,14 @@ for item in range(100):
 
     train_image = Image.fromarray(train_image_array)
     test_image = Image.fromarray(test_image_array)
-    train_image.save(f"train_x_images/{item}.png")
-    test_image.save(f"test_x_images/{item}.png")
+
+    train_path = Path('train_x_images')
+    train_path.mkdir(exist_ok=True)
+    train_image.save(train_path / f"{item}.png")
+
+    test_path = Path('test_x_images')
+    test_path.mkdir(exist_ok=True)
+    test_image.save(test_path / f"{item}.png")
 
     # add as a "pillow_image" to a list
     trainimage_x = Image.open(f"train_x_images/{item}.png")
