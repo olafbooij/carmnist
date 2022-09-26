5875beb42308a96e9d332c145d2e6220b4a0e053
Olivier <109768686+ooinm@users.noreply.github.com>
2022-09-26 12:16:16 +0200

    Update extract_images_mnist_database.py

--- extract_images_mnist_database.py
+++ extract_images_mnist_database.py
@@ -2,33 +2,31 @@ from keras.datasets import mnist
 from PIL import Image, ImageOps
 from pathlib import Path
 
+
+def save_first_100_images(array, path):
+    # Extract and save image as a file
+    images = []
+
+    for item in range(100):
+        # select 'test' or 'train' image from the mnist database
+        image_array = array[item]
+
+        image = Image.fromarray(image_array)
+
+        path = Path(path)
+        path.mkdir(exist_ok=True)
+        image.save(path / f"{item}.png")
+
+        # add as a "pillow_image" to a list
+        image_x = Image.open(f"{path}/{item}.png")
+
+        images.append(image_x)
+
+    return images
+
+
 # Load the mnist dataset (caches in .keras/datasets/mnist.npz)
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
 
-# Extract and save image as a file
-train_images = []
-test_images = []
-for item in range(100):
-
-    #select 'test' and 'train' image from the mnist database
-    train_image_array = train_X[item]
-    test_image_array = test_X[item]
-
-    train_image = Image.fromarray(train_image_array)
-    test_image = Image.fromarray(test_image_array)
-
-    train_path = Path('train_x_images')
-    train_path.mkdir(exist_ok=True)
-    train_image.save(train_path / f"{item}.png")
-
-    test_path = Path('test_x_images')
-    test_path.mkdir(exist_ok=True)
-    test_image.save(test_path / f"{item}.png")
-
-    # add as a "pillow_image" to a list
-    trainimage_x = Image.open(f"train_x_images/{item}.png")
-    testimage_x = Image.open(f"test_x_images/{item}.png")
-    train_images.append(trainimage_x)
-    test_images.append(testimage_x)
-print(train_images)
-print(test_images)
+print(save_first_100_images(array=test_X, path="test_x_images"))
+print(save_first_100_images(array=train_X, path="train_x_images"))
