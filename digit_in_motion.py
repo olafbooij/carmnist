d27425813a498180779d6550ec28baa29144732a
O B thuis <olaf.booij@xs4all.nl>
2022-10-13 17:51:22 +0200

    split essential part from square_in_motion to curved_motion

--- digit_in_motion.py
+++ digit_in_motion.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from PIL import Image
 import numpy
-import square_in_motion
+import curved_motion
 
 
 def digit_in_motion(image_path):
@@ -13,12 +13,12 @@ def digit_in_motion(image_path):
 
 
 def image_in_motion(img_in):
-    square = square_in_motion.random_square_points_3d()
-    for quadrangle in square_in_motion.curved_motion(square):
-        yield transform_image(quadrangle, img_in)
+    img_out_size = (100, 100)
+    for homography in curved_motion.curved_motion_homography(img_in.size, img_out_size):
+        yield transform_image(homography, img_in)
 
 
-def transform_image(quadrangle, img_in):
+def transform_image(homography, img_in):
     img_out = img_in  # TODO
     return img_out
 
--- square_in_motion.py
+++ square_in_motion.py
@@ -1,59 +1,6 @@
-from random import seed, uniform
-from transformations import identity_matrix, euler_matrix, concatenate_matrices, translation_matrix, scale_matrix
+from random import seed
 import numpy
-
-
-# Create trajectory of transformations moving further and further away from the
-# camera. The movement follows a curved path (with fixed random translation and
-# rotation). These transformations are expressed as 4x4 transformation
-# matrices.
-def curved_motion(nr_of_steps):
-  # random camera motion (one step) (position direction close to z)
-  cam_translation = translation_matrix((uniform(.01, .01), uniform(.01, .01), uniform(-1.5, -.5)))
-  # bit of camera rotation (bit of y, even less x)
-  cam_rotation = euler_matrix(uniform(-.001,.001), uniform(-.01, .01), 0., 'rxyz')
-  cam_trans_delta = concatenate_matrices(cam_rotation, cam_translation)
-  # begin with a first step of cam_trans_delta, and then iteratively add
-  # another step.
-  cam_trans_square = cam_trans_delta
-  for distance in range(0, nr_of_steps):
-    cam_trans_square = concatenate_matrices(cam_trans_delta, cam_trans_square)
-    yield cam_trans_square
-
-
-# Use this trajectory to create homographies, expressing an input image in the
-# output image at the end of the trajectory.
-def curved_motion_homography(input_image_size, output_image_size):
-  # Define camera calibration K_in for the input image given an image center
-  # a focal length f_in, based on the size of the image input.
-  f_in = input_image_size[0]
-  K_in_inv = numpy.array([[ 1. / f_in, 0.       , -.5 ],
-                          [ 0.       , 1. / f_in, -.5 ],
-                          [ 0.       , 0.       , 1.  ]])
-  # Define camera calibration K_out for the output image given an image center
-  # c_out and focal length f_out, both based on the size of hte output image.
-  c_out = output_image_size[0]//2
-  f_out = output_image_size[0]
-  K_out = numpy.array([[f_out, 0.   , c_out],
-                       [0.   , f_out, c_out],
-                       [0.   , 0.   , 1.   ]]);
-  # Define position of input image relative to the start of the trajectory
-  start_trans_in_image = translation_matrix((uniform(-1., 1.), uniform(-1., 1.), 0))
-  for transformation in curved_motion(32):
-    # Concatenate the start with the transformation of the trajectory
-    homography = concatenate_matrices(transformation, start_trans_in_image)
-    # From the 4x4 transformation matrix create a 3x3 "Homography matrix". This
-    # requires some "magic": adding the 4rd column (translation) to the 3rd
-    # column (z-axis rotation), and keep only upper-left 3x3 part.
-    homography[:,2] += homography[:,3]
-    homography = homography[0:3, 0:3]
-    # Apply th camera calibrations
-    homography = numpy.dot(homography, K_in_inv)
-    homography = numpy.dot(K_out, homography)
-    yield homography
-
-
-seed(2)
+import curved_motion
 
 # let's use this trajectory of homographies to plot the trajectory of a square.
 
@@ -73,7 +20,7 @@ def project_point_homography(transformation, point):
 
 
 def curved_square_motion(square, input_image_size, output_image_size):
-  for cam_homography_square in curved_motion_homography(input_image_size, output_image_size):
+  for cam_homography_square in curved_motion.curved_motion_homography(input_image_size, output_image_size):
     yield [project_point_homography(cam_homography_square, point) for point in square]
 
 
@@ -81,6 +28,8 @@ def print_square(square):
   for point in square + [square[0]]:
     print(point[0], point[1])
   print()
+
+seed(2)
 
 input_image_size = (28, 28)
 output_image_size = (100, 100)
