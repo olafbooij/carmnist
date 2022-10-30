2e6ef5de38f1c651d236babf4c8a3769dcfcf032
O B thuis <olaf.booij@xs4all.nl>
2022-10-30 16:08:41 +0100

    random mnist image adds choice for testset

--- load_mnist_image.py
+++ load_mnist_image.py
@@ -2,8 +2,9 @@ from keras.datasets import mnist
 from PIL import Image
 import random
 
-def load_random_mnist_image():
-    (train_X, _), _ = mnist.load_data()
-    image_array = train_X[random.randint(0, len(train_X))]
+def load_random_mnist_image(use_test_set=False):
+    (trainset, testset), _ = mnist.load_data()
+    dataset = testset if use_test_set else trainset
+    image_array = random.choice(dataset)
     image = Image.fromarray(image_array)
     return image
