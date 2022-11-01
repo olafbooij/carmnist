3d1e876cc0d39c8a9f5483c3f9ae7fc5bd07af19
Olivier <109768686+ooinm@users.noreply.github.com>
2022-11-01 23:33:50 +0100

    Update digit_in_motion.py

--- digit_in_motion.py
+++ digit_in_motion.py
@@ -9,8 +9,6 @@ SIZE = (100, 100)
 
 
 def digit_in_motion(image_path):
-    # path_out = Path("digit_in_motion")
-    # path_out.mkdir(exist_ok=True)
     img_in = Image.open(image_path)
     for item, img_out in enumerate(image_in_motion(img_in)):
         if item < 10:
