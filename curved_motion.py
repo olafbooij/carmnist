d110da3908f659adea2bd600a2ae630cd5dea6dc
O B thuis <olaf.booij@xs4all.nl>
2022-11-02 23:30:04 +0100

    a bit more curve in the motion

--- curved_motion.py
+++ curved_motion.py
@@ -9,9 +9,9 @@ import numpy
 # matrices.
 def curved_motion(nr_of_steps):
   # random camera motion (one step) (position direction close to z)
-  cam_translation = translation_matrix((uniform(.01, .01), uniform(.01, .01), uniform(.5, 1.5)))
+  cam_translation = translation_matrix((uniform(.01, .01), uniform(.01, .01), uniform(.2, 1.0)))
   # bit of camera rotation (bit of y, even less x)
-  cam_rotation = euler_matrix(uniform(-.001,.001), uniform(-.01, .01), 0., 'rxyz')
+  cam_rotation = euler_matrix(uniform(-.002,.002), uniform(-.05, .05), 0., 'rxyz')
   cam_trans_delta = concatenate_matrices(cam_rotation, cam_translation)
   # begin with a first step of cam_trans_delta, and then iteratively add
   # another step.
